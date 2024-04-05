72eb5bdf7a4a7bae8a1336c323944aeb661d1368
Sxnthxshpxndi <mynameissanthoshpandi.gmail.com>
2024-04-05 11:59:01 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share AI Prompts
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open source AI prompting tool for the modern world to
+        discover, create, and share prompts
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
